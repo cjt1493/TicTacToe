5010b47d9a6fb00fb03d9776de638bf67ed74f84
cjt1493 <60484829+cjt1493@users.noreply.github.com>
2020-09-21 01:29:49 -0400

    Base file for TicTacToe

---
+++ src/TicTacToe.java
@@ -0,0 +1,14 @@
+public class TicTacToe{
+
+    private int x;
+    private int o;
+
+    public TicTacToe(int x, int o){
+        o = o;
+        x = x;
+    }
+
+    public static void main(String[] args){
+        System.out.println("Test.")
+    }
+}
